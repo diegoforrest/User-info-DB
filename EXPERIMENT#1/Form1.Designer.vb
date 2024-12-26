79b97b7f75ffb9b249554a37f1bf2aeb26b1b4ff
diegoforrest <cruzdieeego@gmail.com>
2024-12-26 22:06:59 +0800

    Add project files.

---
+++ EXPERIMENT#1/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.FIRST_NAME = New System.Windows.Forms.Label()
+        Me.MIDDLE_NAME = New System.Windows.Forms.Label()
+        Me.LAST_NAME = New System.Windows.Forms.Label()
+        Me.NICK_NAME = New System.Windows.Forms.Label()
+        Me.Guna2ToggleSwitch1 = New Guna.UI2.WinForms.Guna2ToggleSwitch()
+        Me.NICKNAME = New EXPERIMENT_1.RoundCorner()
+        Me.LASTNAME = New EXPERIMENT_1.RoundCorner()
+        Me.MIDDLENAME = New EXPERIMENT_1.RoundCorner()
+        Me.FIRSTNAME = New EXPERIMENT_1.RoundCorner()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold)
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(581, 484)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 31)
+        Me.Button2.TabIndex = 12
+        Me.Button2.Text = "Register"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'FIRST_NAME
+        '
+        Me.FIRST_NAME.AutoSize = True
+        Me.FIRST_NAME.BackColor = System.Drawing.Color.Transparent
+        Me.FIRST_NAME.Font = New System.Drawing.Font("Sitka Banner", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.FIRST_NAME.ForeColor = System.Drawing.Color.Black
+        Me.FIRST_NAME.Location = New System.Drawing.Point(305, 203)
+        Me.FIRST_NAME.Name = "FIRST_NAME"
+        Me.FIRST_NAME.Size = New System.Drawing.Size(130, 35)
+        Me.FIRST_NAME.TabIndex = 13
+        Me.FIRST_NAME.Text = "First Name:"
+        '
+        'MIDDLE_NAME
+        '
+        Me.MIDDLE_NAME.AutoSize = True
+        Me.MIDDLE_NAME.BackColor = System.Drawing.Color.Transparent
+        Me.MIDDLE_NAME.Font = New System.Drawing.Font("Sitka Banner", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.MIDDLE_NAME.ForeColor = System.Drawing.Color.Black
+        Me.MIDDLE_NAME.Location = New System.Drawing.Point(305, 269)
+        Me.MIDDLE_NAME.Name = "MIDDLE_NAME"
+        Me.MIDDLE_NAME.Size = New System.Drawing.Size(150, 35)
+        Me.MIDDLE_NAME.TabIndex = 14
+        Me.MIDDLE_NAME.Text = "Middle Name:"
+        '
+        'LAST_NAME
+        '
+        Me.LAST_NAME.AutoSize = True
+        Me.LAST_NAME.BackColor = System.Drawing.Color.Transparent
+        Me.LAST_NAME.Font = New System.Drawing.Font("Sitka Banner", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.LAST_NAME.ForeColor = System.Drawing.Color.Black
+        Me.LAST_NAME.Location = New System.Drawing.Point(305, 335)
+        Me.LAST_NAME.Name = "LAST_NAME"
+        Me.LAST_NAME.Size = New System.Drawing.Size(124, 35)
+        Me.LAST_NAME.TabIndex = 15
+        Me.LAST_NAME.Text = "Last Name:"
+        '
+        'NICK_NAME
+        '
+        Me.NICK_NAME.AutoSize = True
+        Me.NICK_NAME.BackColor = System.Drawing.Color.Transparent
+        Me.NICK_NAME.Font = New System.Drawing.Font("Sitka Banner", 14.0!, System.Drawing.FontStyle.Bold)
+        Me.NICK_NAME.ForeColor = System.Drawing.Color.Black
+        Me.NICK_NAME.Location = New System.Drawing.Point(305, 401)
+        Me.NICK_NAME.Name = "NICK_NAME"
+        Me.NICK_NAME.Size = New System.Drawing.Size(121, 35)
+        Me.NICK_NAME.TabIndex = 16
+        Me.NICK_NAME.Text = "Nickname:"
+        '
+        'Guna2ToggleSwitch1
+        '
+        Me.Guna2ToggleSwitch1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ToggleSwitch1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2ToggleSwitch1.CheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2ToggleSwitch1.CheckedState.InnerBorderColor = System.Drawing.Color.White
+        Me.Guna2ToggleSwitch1.CheckedState.InnerColor = System.Drawing.Color.White
+        Me.Guna2ToggleSwitch1.Location = New System.Drawing.Point(434, 484)
+        Me.Guna2ToggleSwitch1.Name = "Guna2ToggleSwitch1"
+        Me.Guna2ToggleSwitch1.Size = New System.Drawing.Size(77, 31)
+        Me.Guna2ToggleSwitch1.TabIndex = 26
+        Me.Guna2ToggleSwitch1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(125, Byte), Integer), CType(CType(137, Byte), Integer), CType(CType(149, Byte), Integer))
+        Me.Guna2ToggleSwitch1.UncheckedState.FillColor = System.Drawing.Color.FromArgb(CType(CType(125, Byte), Integer), CType(CType(137, Byte), Integer), CType(CType(149, Byte), Integer))
+        Me.Guna2ToggleSwitch1.UncheckedState.InnerBorderColor = System.Drawing.Color.White
+        Me.Guna2ToggleSwitch1.UncheckedState.InnerColor = System.Drawing.Color.White
+        '
+        'NICKNAME
+        '
+        Me.NICKNAME.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer))
+        Me.NICKNAME.Font = New System.Drawing.Font("Sitka Banner", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.NICKNAME.Location = New System.Drawing.Point(471, 398)
+        Me.NICKNAME.Name = "NICKNAME"
+        Me.NICKNAME.Size = New System.Drawing.Size(277, 36)
+        Me.NICKNAME.TabIndex = 24
+        Me.NICKNAME.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'LASTNAME
+        '
+        Me.LASTNAME.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer))
+        Me.LASTNAME.Font = New System.Drawing.Font("Sitka Banner", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LASTNAME.Location = New System.Drawing.Point(471, 332)
+        Me.LASTNAME.Name = "LASTNAME"
+        Me.LASTNAME.Size = New System.Drawing.Size(277, 36)
+        Me.LASTNAME.TabIndex = 23
+        Me.LASTNAME.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'MIDDLENAME
+        '
+        Me.MIDDLENAME.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer))
+        Me.MIDDLENAME.Font = New System.Drawing.Font("Sitka Banner", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MIDDLENAME.Location = New System.Drawing.Point(471, 266)
+        Me.MIDDLENAME.Name = "MIDDLENAME"
+        Me.MIDDLENAME.Size = New System.Drawing.Size(277, 36)
+        Me.MIDDLENAME.TabIndex = 22
+        Me.MIDDLENAME.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'FIRSTNAME
+        '
+        Me.FIRSTNAME.BackColor = System.Drawing.Color.FromArgb(CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer), CType(CType(234, Byte), Integer))
+        Me.FIRSTNAME.Font = New System.Drawing.Font("Sitka Banner", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FIRSTNAME.ForeColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.FIRSTNAME.Location = New System.Drawing.Point(471, 200)
+        Me.FIRSTNAME.Name = "FIRSTNAME"
+        Me.FIRSTNAME.Size = New System.Drawing.Size(277, 36)
+        Me.FIRSTNAME.TabIndex = 21
+        Me.FIRSTNAME.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1094, 602)
+        Me.Controls.Add(Me.Guna2ToggleSwitch1)
+        Me.Controls.Add(Me.NICKNAME)
+        Me.Controls.Add(Me.LASTNAME)
+        Me.Controls.Add(Me.MIDDLENAME)
+        Me.Controls.Add(Me.FIRSTNAME)
+        Me.Controls.Add(Me.NICK_NAME)
+        Me.Controls.Add(Me.LAST_NAME)
+        Me.Controls.Add(Me.MIDDLE_NAME)
+        Me.Controls.Add(Me.FIRST_NAME)
+        Me.Controls.Add(Me.Button2)
+        Me.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowInTaskbar = False
+        Me.Text = "T-CPET411LA EXPERIMENT #1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents FIRST_NAME As Label
+    Friend WithEvents MIDDLE_NAME As Label
+    Friend WithEvents LAST_NAME As Label
+    Friend WithEvents NICK_NAME As Label
+    Friend WithEvents FIRSTNAME As RoundCorner
+    Friend WithEvents MIDDLENAME As RoundCorner
+    Friend WithEvents LASTNAME As RoundCorner
+    Friend WithEvents NICKNAME As RoundCorner
+    Friend WithEvents Guna2ToggleSwitch1 As Guna.UI2.WinForms.Guna2ToggleSwitch
+End Class
---
+++ EXPERIMENT#1/Form1.vb
@@ -0,0 +1,72 @@
+﻿Imports Guna.UI2.WinForms
+Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private conn As New MySqlConnection
+    Private CMD As MySqlCommand
+    Dim CONNECT As MySqlConnection
+    Dim CONSTRING As String = "DATA SOURCE = localhost; USERID= root; password= ; DATABASE = experiment1"
+    Dim COMMAND As MySqlCommand
+
+    Private Sub Guna2ToggleSwitch1_CheckedChanged_1(sender As Object, e As EventArgs) Handles Guna2ToggleSwitch1.CheckedChanged
+        Try
+            If Guna2ToggleSwitch1.Checked = True Then
+                CONNECT = New MySqlConnection(CONSTRING)
+                CONNECT.Open()
+                Button2.Enabled = True
+                MsgBox("Connected to Database Experiment1", vbInformation, "Confirmation Window")
+            Else
+                If CONNECT IsNot Nothing AndAlso CONNECT.State = ConnectionState.Open Then
+                    CONNECT.Close()
+                    Button2.Enabled = False
+                    MsgBox("Disconnected from Database Experiment1", vbInformation, "Confirmation Window")
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            If Not FIRSTNAME.Text = "" Then
+                CONNECT = New MySqlConnection(CONSTRING)
+                CONNECT.Open()
+                Dim SQL As String = "INSERT INTO experiment1 (FIRST_NAME, MIDDLE_NAME, LAST_NAME, NICK_NAME) values ('" & FIRSTNAME.Text & "' , '" & MIDDLENAME.Text & "' , '" & LASTNAME.Text & "', '" & NICKNAME.Text & "')"
+                COMMAND = New MySqlCommand(SQL, CONNECT)
+                Dim i As Integer = COMMAND.ExecuteNonQuery
+
+                FIRSTNAME.Text = " "
+                LASTNAME.Text = " "
+                MIDDLENAME.Text = " "
+                NICKNAME.Text = " "
+
+                If i <> 0 Then
+                    MsgBox("Record Saved", vbInformation, "Admin")
+                Else
+                    MsgBox("Record Not Saved", vbCritical, "Admin")
+                End If
+                CONNECT.Close()
+            Else
+                MsgBox("Invalid")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        Button2.Enabled = False
+        CONNECT.Close()
+        MsgBox("Successfully Connected to Database experiment1", vbInformation, "Confirmation Window")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button2.Enabled = False
+    End Sub
+
+
+End Class
